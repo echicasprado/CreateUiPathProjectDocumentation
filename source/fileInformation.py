cdc8710983caf4dbe1e3fd23dca26fa1e4e96e6d
echicasprado <everchicasp@outlook.com>
2023-09-03 06:04:28 -0600

    add readXaml data

--- source/fileInformation.py
+++ source/fileInformation.py
@@ -3,3 +3,9 @@ class xamlFile:
         self.path = path
         self.description = description
         self.arguments = arguments
+        
+    def __str__(self) -> str:
+        args = ""
+        for arg in self.arguments:
+            args = f'{args}\n\t{arg}'
+        return f'File path: {self.path}\nDescription: {self.description}\nArguments: {args}'
--- source/readXaml.py
+++ source/readXaml.py
@@ -1,24 +1,70 @@
 from bs4 import BeautifulSoup
 from fileInformation import xamlFile
 
+FILES_TYPE_UIPATH = ["Flowchart","StateMachine","Sequence"]
+XML_DATA = None
 
 # file path to open
-filePath = '/Users/everchicas/GIT/ReadPath/test/ActivacionEquiposServiciosFijosGT/Main.xaml'
-with open(filePath, 'r') as f:
-	data = f.read()
+TestFilePath = '/Users/everchicas/GIT/ReadPath/test/ActivacionEquiposServiciosFijosGT/Main.xaml'
+#with open(filePath, 'r') as f:
+#	data = f.read()
 
 # Passing the stored data inside
 # the beautifulsoup parser, storing
 # the returned object
-Bs_data = BeautifulSoup(data, "xml")
+#Bs_data = BeautifulSoup(data, "xml")
 
 # Finding all instances of tag
 # x:Property
-b_propertys = Bs_data.find_all('x:Property')
+# b_propertys = Bs_data.find_all('x:Property')
+#b_type = Bs_data.find("Flowchart")
+
+# for currentType in FILES_TYPE_UIPATH:
+# 	CURRENT_FILE_TYPE = Bs_data.find(currentType)
+# 	if(CURRENT_FILE_TYPE == None):
+# 		continue
+# 	else:
+# 		print("###########################")
+# 		print(f'Current file type: {currentType}\nContent: {CURRENT_FILE_TYPE.get("sap2010:Annotation.AnnotationText")}')
+# 		print("###########################")
+# 		break
 
 # Show arguments name
-for member in b_propertys:
+#for member in b_propertys:
 	#name = member.get('Name')
 	#nameTag = BeautifulSoup(member,'xml.parser')
-	print(member.get('Name'))
+	#print(member.get('Name'))
 
+def openFile(filePath):
+	with open(filePath, 'r') as f:
+		data = f.read()
+	return BeautifulSoup(data, "xml")
+ 
+def getDescription(XML_DATA):
+	for currentType in FILES_TYPE_UIPATH:
+		currentFileType = XML_DATA.find(currentType)
+		if(currentFileType == None):
+			continue
+		else:
+			print("###########################")
+			print(f'Current file type: {currentType}\nContent: {currentFileType.get("sap2010:Annotation.AnnotationText")}')
+			print("###########################")
+			return currentFileType.get("sap2010:Annotation.AnnotationText")
+
+def getArguments(XML_DATA):
+	arrayProperty = []
+	currentArguments = XML_DATA.find_all('x:Property')
+	for arg in currentArguments:
+		arrayProperty.append(arg.get('Name'))
+	return arrayProperty
+	
+def main(filePath):
+	XML_DATA = openFile(filePath)
+	informationXaml = xamlFile("","",[])
+	informationXaml.description = getDescription(XML_DATA)
+	informationXaml.arguments = getArguments(XML_DATA)
+	informationXaml.path = filePath
+	print(str(informationXaml))
+    
+if __name__ == "__main__":
+	main(TestFilePath)
