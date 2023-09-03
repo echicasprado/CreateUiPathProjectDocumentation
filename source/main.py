ae989e5488dec073e12cca4c63f94a8ef6dc20f4
echicasprado <everchicasp@outlook.com>
2023-09-03 06:15:46 -0600

    update main and readXaml

--- source/main.py
+++ source/main.py
@@ -5,11 +5,6 @@ def useOS(filePath,rootFolderPath):
     currentFilePath = os.path.abspath(filePath)
     currentFilePathFromRootFolder = currentFilePath.replace(rootFolderPath, "")
     print(currentFilePathFromRootFolder)
-
-def readFile(filePath):
-    file = open(filePath,'r')
-    [print(line) for line in file.readlines()]
-    file.close()
     
 def getAllFiles(rootFolderPath):
     rootFolder = rootFolderPath + "/**/*.*"
@@ -19,7 +14,7 @@ def getAllFiles(rootFolderPath):
 
 def main():
     rootFolderPath = input("Ingresar ruta del proyecto: ")
-    readFile(rootFolderPath)
+    getAllFiles(rootFolderPath)
     
 if __name__ == "__main__":
     main()
--- source/readXaml.py
+++ source/readXaml.py
@@ -1,39 +1,14 @@
 from bs4 import BeautifulSoup
 from fileInformation import xamlFile
 
-FILES_TYPE_UIPATH = ["Flowchart","StateMachine","Sequence"]
-XML_DATA = None
 
-# file path to open
-TestFilePath = '/Users/everchicas/GIT/ReadPath/test/ActivacionEquiposServiciosFijosGT/Main.xaml'
-#with open(filePath, 'r') as f:
-#	data = f.read()
-
-# Passing the stored data inside
-# the beautifulsoup parser, storing
-# the returned object
-#Bs_data = BeautifulSoup(data, "xml")
-
-# Finding all instances of tag
-# x:Property
-# b_propertys = Bs_data.find_all('x:Property')
-#b_type = Bs_data.find("Flowchart")
-
-# for currentType in FILES_TYPE_UIPATH:
-# 	CURRENT_FILE_TYPE = Bs_data.find(currentType)
-# 	if(CURRENT_FILE_TYPE == None):
-# 		continue
-# 	else:
-# 		print("###########################")
-# 		print(f'Current file type: {currentType}\nContent: {CURRENT_FILE_TYPE.get("sap2010:Annotation.AnnotationText")}')
-# 		print("###########################")
-# 		break
-
-# Show arguments name
-#for member in b_propertys:
-	#name = member.get('Name')
-	#nameTag = BeautifulSoup(member,'xml.parser')
-	#print(member.get('Name'))
+TestFilePath = [
+	'/Users/everchicas/GIT/ReadPath/test/UseReThrow/Test/Flowchart.xaml',
+	'/Users/everchicas/GIT/ReadPath/test/UseReThrow/Test/Sequence.xaml',
+	'/Users/everchicas/GIT/ReadPath/test/UseReThrow/Test/StateMachine.xaml',
+	'/Users/everchicas/GIT/ReadPath/test/UseReThrow/Test/Workflow.xaml',
+ 	'/Users/everchicas/GIT/ReadPath/test/UseReThrow/Test/WorkflowWithTemplate.xaml'
+]
 
 def openFile(filePath):
 	with open(filePath, 'r') as f:
@@ -41,19 +16,22 @@ def openFile(filePath):
 	return BeautifulSoup(data, "xml")
  
 def getDescription(XML_DATA):
+	FILES_TYPE_UIPATH = ["Flowchart","StateMachine","Sequence"]
+ 
 	for currentType in FILES_TYPE_UIPATH:
 		currentFileType = XML_DATA.find(currentType)
 		if(currentFileType == None):
 			continue
 		else:
-			print("###########################")
-			print(f'Current file type: {currentType}\nContent: {currentFileType.get("sap2010:Annotation.AnnotationText")}')
-			print("###########################")
+			# print("###########################")
+			# print(f'Current file type: {currentType}\nContent: {currentFileType.get("sap2010:Annotation.AnnotationText")}')
+			# print("###########################")
 			return currentFileType.get("sap2010:Annotation.AnnotationText")
 
 def getArguments(XML_DATA):
 	arrayProperty = []
 	currentArguments = XML_DATA.find_all('x:Property')
+ 
 	for arg in currentArguments:
 		arrayProperty.append(arg.get('Name'))
 	return arrayProperty
@@ -67,4 +45,7 @@ def main(filePath):
 	print(str(informationXaml))
     
 if __name__ == "__main__":
-	main(TestFilePath)
+	for temp in TestFilePath:
+		print('-----------------------------------------')
+		main(temp)
+		print('-----------------------------------------')
